19fcdd8a2b0345427dbb0be66ff897ba6420d233
Ben Pate <ben@pate.org>
2026-02-21 08:30:20 -0700

    Migrate code to new repository

---
+++ user-conversations/resources/app/src/model/actor.ts
@@ -0,0 +1,10 @@
+export type Actor = {
+	id: string
+	name: string
+	username: string
+	icon: string
+	inbox: string
+	outbox: string
+	mlsInbox: string
+	keyPackages: string
+}
---
+++ user-conversations/resources/app/src/model/message.ts
@@ -0,0 +1,7 @@
+export type Message = {
+	id: string
+	group: string
+	sender: string
+	plaintext: string
+	createDate: number
+}
