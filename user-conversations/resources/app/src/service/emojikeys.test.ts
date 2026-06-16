07b4fedba1641fd479b6eddb14665915887cdee6
Ben Pate <ben@pate.org>
2026-06-16 10:45:58 -0600

    Fix tests for emojikeys signatures

--- user-conversations/resources/app/src/service/emojikeys.test.ts
+++ user-conversations/resources/app/src/service/emojikeys.test.ts
@@ -4,11 +4,17 @@ import { emojiKey } from "./emojikeys"
 test('test key A => Mosque, Watermelon, Juice Box, X-Ray, Peanut', async () => {
 	const buffer = new TextEncoder().encode("test key A")
 	const result = await emojiKey(buffer)
-	expect(result).toEqual([["🕌", "Mosque"], ["🍉", "Watermelon"], ["🧃", "Juice Box"], ["🩻", "X-Ray"], ["🥜", "Peanut"]])
+	expect(result).toEqual([
+		"4N0Pcss20QcSn4D64OfooibR56PTnawKNfBlO/yxkIU=",
+		[["🕌", "Mosque"], ["🍉", "Watermelon"], ["🧃", "Juice Box"], ["🩻", "X-Ray"], ["🥜", "Peanut"]],
+	])
 })
 
 test('test key 2 => Full Moon, Globe, Canoe, Teapot, Peacock', async () => {
 	const buffer = new TextEncoder().encode("test key 2")
 	const result = await emojiKey(buffer)
-	expect(result).toEqual([["🌕", "Full Moon"], ["🌏", "Globe"], ["🛶", "Canoe"], ["🫖", "Teapot"], ["🦚", "Peacock"]])
+	expect(result).toEqual([
+		"kOGm3x/6VEKfHIHYUW1uecf9by3yQV86t5SzTcgOQHQ=",
+		[["🌕", "Full Moon"], ["🌏", "Globe"], ["🛶", "Canoe"], ["🫖", "Teapot"], ["🦚", "Peacock"]],
+	])
 })
